82855a39d58c34c238fb49c780c6ffa48cad8dc6
seansio1995 <seansio@163.com>
2017-09-13 22:40:44 -0400

    first commit

---
+++ regional2010/pennyGame.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int result[8]={0};
+int main()
+{
+  int num_lines;
+  int index;
+  int penny;
+  cin>>num_lines;
+
+  for (int i=0;i<num_lines;i++){
+    cin>>index;
+    string str;
+    cin>>str;
+
+    for (int j=0;j<8;j++){
+      result[j]=0;
+
+    }
+    for (int k=0;k<38;k++){
+      
+    }
+  }
+}
